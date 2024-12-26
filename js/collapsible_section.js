325bcdc9f63c38eee04b5b149beabbeea8964932
Bradley Hampton <BradleyCDHampton@gmail.com>
2024-12-26 01:23:11 -0500

    Added dropdowns for Projects

---
+++ js/collapsible_section.js
@@ -0,0 +1,18 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+console.log(coll.length);
+console.log("loaded");
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    console.log("Button pressed!");
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
